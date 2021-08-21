1d889bd22dc547a76c295aee0bfc2880ca2589e7
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2021-08-21 13:19:05 +0100

    Reset repository to Purpur 1.17.1
    Reset and rebase on Purpur at commit c82d29ade8922d052798019c8975ee31d02fb41b. Since Purpur has re-included Airplane's patches, this will make it much easier to strip down their patches.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.8"
+    id("io.papermc.paperweight.patcher") version "1.1.9"
 }
 
 repositories {
@@ -46,9 +46,8 @@ subprojects {
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://nexus.velocitypowered.com/repository/velocity-artifacts-snapshots/")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://jitpack.io")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
 
     configure<PublishingExtension> {
@@ -61,15 +60,15 @@ subprojects {
 }
 
 paperweight {
-    serverProject.set(project(":Purepur-Server"))
+    serverProject.set(project(":Purpur-Server"))
 
     usePaperUpstream(providers.gradleProperty("paperCommit")) {
         withPaperPatcher {
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("Purepur-API"))
+            apiOutputDir.set(layout.projectDirectory.dir("Purpur-API"))
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-            serverOutputDir.set(layout.projectDirectory.dir("Purepur-Server"))
+            serverOutputDir.set(layout.projectDirectory.dir("Purpur-Server"))
         }
     }
 }
