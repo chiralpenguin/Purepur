67d3de9fe7dc2e7eca34b899ea0aa9adb9573b87
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2022-05-16 17:14:14 +0100

    Rebuild Purepur for 1.18
    Rebuild Purepur on top of Purpur commit 259a9eec4a2928370b1ccf19308ac65f8ce203bd for 1.18. Some new patches have been added in this build, in addition to all of Pufferfish's changes.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Purepur
-Purepur is a fork of [Purpur](https://github.com/pl3xgaming/Purpur) and includes a hand-picked selection of its patches as well as some custom changes specifically for Purity Vanilla. Purepur also includes all of [Tuinity](https://github.com/SpottedLeaf/Tuinity) and [Airplane's](https://github.com/TECHNOVE/Airplane) patches, which are combined in Purpur.
+Purepur is a fork of [Purpur](https://github.com/PurpurMC/Purpur) which only includes a small selection of patches, specific for Purity Vanilla. This includes all of Pufferfish's changes.
 
 As this fork is maintained soley for use by the Purity Network, you will not receive any support. However, you are free to use our fork and submit contributions via PRs.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,50 @@
-import io.papermc.paperweight.util.constants.*
+import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.2.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("io.papermc.paperweight.patcher") version "1.3.6"
 }
 
 repositories {
     mavenCentral()
     maven("https://papermc.io/repo/repository/maven-public/") {
-        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+        content {
+            onlyForConfigurations(PAPERCLIP_CONFIG)
+        }
     }
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.498.12")
-    paperclip("io.papermc:paperclip:2.0.1")
+    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.29")
+    paperclip("io.papermc:paperclip:3.0.2")
 }
 
-subprojects {
+allprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(16))
+            languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
+}
 
+subprojects {
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
-        options.release.set(16)
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
     }
 
     repositories {
@@ -43,21 +55,13 @@ subprojects {
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://jitpack.io")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    }
-
-    configure<PublishingExtension> {
-        repositories.maven {
-            name = "maven"
-            url = uri("https://repo.pl3x.net/snapshots")
-            credentials(PasswordCredentials::class)
-        }
+        maven("https://jitpack.io")
     }
 }
 
 paperweight {
-    serverProject.set(project(":Purpur-Server"))
+    serverProject.set(project(":purpur-server"))
 
     remapRepo.set("https://maven.fabricmc.net/")
     decompileRepo.set("https://files.minecraftforge.net/maven/")
@@ -69,6 +73,39 @@ paperweight {
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Purpur-Server"))
+        }
+    }
+}
+
+tasks.generateDevelopmentBundle {
+    apiCoordinates.set("org.purpurmc.purpur:purpur-api")
+    mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
+    libraryRepositories.set(
+        listOf(
+            "https://repo.maven.apache.org/maven2/",
+            "https://libraries.minecraft.net/",
+            "https://papermc.io/repo/repository/maven-public/",
+            "https://maven.quiltmc.org/repository/release/",
+            "https://repo.purpurmc.org/snapshots",
+        )
+    )
+}
+
+allprojects {
+    publishing {
+        repositories {
+            maven("https://repo.purpurmc.org/snapshots") {
+                name = "purpur"
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("devBundle") {
+        artifact(tasks.generateDevelopmentBundle) {
+            artifactId = "dev-bundle"
         }
     }
 }
